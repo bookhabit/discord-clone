505d24a5c1ff572b76fd69fbdf02a8f9a2a2da02
bookhabit <qnrgoqlt@naver.com>
2023-12-08 00:17:03 +0900

    leaveServer 모달 , 서버 나가기 기능

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -72,7 +72,9 @@ export const ServerHeader = ({
                 )}
                 {/* isAdmin - 서버주인이 '나'가 아닐 때 */}
                 {!isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        onClick={()=>onOpen("leaveServer",{server})}
+                        className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
